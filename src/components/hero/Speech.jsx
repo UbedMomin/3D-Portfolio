433d46de488b50cc0786ff6b3e40152079869712
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-08-28 23:12:17 +0530

    fixing the animation box

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Speech = () => {
   return (
-    <div className='bubbleContainer'>
-      <div className="bubble">Text</div>
+    <div className="bubbleContainer">
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            //
+            1000,
+            " Same substring at the start",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "We produce food for Hamsters ",
+            1000,
+          ]}
+          wrapper="span"
+          speed={60}
+          deletionSpeed={70}
+          // style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
+        />
+      </div>
       <img src="/man.png" alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Speech
+export default Speech;
